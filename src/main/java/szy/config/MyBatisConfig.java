77167a4a575f9415c7616317d76432bbf11ddc8c
sun <2531>
2026-03-10 10:15:39 +0800

    删除里配置里面的重复扫描

--- src/main/java/szy/config/MyBatisConfig.java
+++ src/main/java/szy/config/MyBatisConfig.java
@@ -1,7 +1,6 @@
 package szy.config;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -10,8 +9,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
+
 @Configuration
-@MapperScan("szy.mapper")
 public class MyBatisConfig {
 
     // 手动创建SqlSessionFactory Bean
